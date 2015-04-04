8fe7e688b091a5f1a71ddfa506151227410af8ad
neupaneshapath <neupaneshapath@gmail.com>
2015-04-04 09:14:23 +0545

    Final Release

--- files/symlinks.sh
+++ files/symlinks.sh
@@ -7,7 +7,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-#Sublime Text Symlink After Install from Cask 
+#Sublime Text Symlink After Install from Cask
 #Sublime Text Install Location ~/Application
 echo "Creating Symlink for Sublime Text 3"
 echo "Check Symlink Location"
@@ -15,6 +15,6 @@ echo ""
 
 ln -s "/Users/shapath/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sl
 
-echo "Create Symlink for bsah_profile"
+echo "Create Symlink for bash_profile"
 
 >/dev/null 2>&1 &
